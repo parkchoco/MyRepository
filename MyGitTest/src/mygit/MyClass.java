f0d4c149c25cc8810dba95893f01b3ae1f2d368b
parkchoco <psy0881@naver.com>
2024-12-05 13:59:44 +0900

    First commit in dev branch

--- MyGitTest/src/mygit/MyClass.java
+++ MyGitTest/src/mygit/MyClass.java
@@ -3,6 +3,8 @@ package mygit;
 public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
-		System.out.println("Hello Git!");
+		System.out.println("This is the 2nd version.");
+		YourClassjava your = new YourClassjava();
+		your.sayHello("Everyone");
 	}
 }
